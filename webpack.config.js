764750d6afda24f5a5324e77560edc55608e1213
Andre <andreferreira@live.co.uk>
2022-09-12 22:15:43 +0100

    webpack-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    devMiddleware: {
+      publicPath: "/assets/",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
